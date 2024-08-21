cebae83f6356b54688a29122a2a2fe9a33e5c9bf
TEE2DWHY <isaacolorunfemi330@gmail.com>
2024-08-21 21:30:22 +0100

    updated the navbar

--- src/Reusable/Navbar/Navbar.tsx
+++ src/Reusable/Navbar/Navbar.tsx
@@ -1,20 +1,45 @@
+import { useEffect, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
 import styles from "./navbar.module.scss";
-import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const location = useLocation();
+  const [activeRoute, setActiveRoute] = useState(location.pathname);
+
+  useEffect(() => {
+    setActiveRoute(location.pathname);
+  }, [location.pathname]);
+
+  const routes = [
+    { path: "/", label: "Home" },
+    { path: "/about", label: "About" },
+    { path: "/product", label: "Product" },
+    { path: "/contact", label: "Contact Us" },
+  ];
+
   return (
-    <>
-      <nav className={styles.navbar}>
-        <span>Logo</span>
-        <ul>
-          <Link to="/about">
-            <li>About</li>
-          </Link>
-          <li>Product</li>
-          <li>Contact Us</li>
-        </ul>
-      </nav>
-    </>
+    <nav className={styles.navbar}>
+      <span>
+        <img
+          src="https://res.cloudinary.com/dg7zsjimc/image/upload/v1724250698/Agribase_Consult_Icon_iaeqdm.svg"
+          alt="logo"
+          className={styles.logo}
+        />
+        Agribase Consult
+      </span>
+      <ul>
+        {routes.map((route) => (
+          <li
+            key={route.path}
+            className={`${activeRoute === route.path ? styles.active : ""}`}
+          >
+            <Link to={route.path} onClick={() => setActiveRoute(route.path)}>
+              {route.label}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
   );
 };
 
