160b0c620673a13de8747c2975be5789e8e57465
Gizzhy <gizzhyakinz@gmail.com>
2024-08-22 17:28:19 +0100

    second commit

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,3 +1,14 @@
+import AboutHero from "./components/AboutHero";
+import AboutBody2 from "./components/AboutBody2";
+import AboutBody1 from "./components/AboutBody1";
+import Navbar from "../../Reusable/Navbar/Navbar";
+import Footer from "../../Reusable/Footer/Footer";
+
 export default function About() {
-  return <>About Page</>;
+  return <>
+           <Navbar/>
+           <AboutBody1/>
+           <AboutBody2/>
+           <Footer/>
+    </>;
 }
---
+++ src/pages/About/components/AboutBody1.tsx
@@ -0,0 +1,19 @@
+import styles from "../about.module.scss"
+
+
+export default function AboutBody1 () 
+{
+    return (
+        <>
+         <div className={styles.secondPart}>
+                <div className={styles.secondPart1}>
+                    <div className={styles.secondPart01}></div>
+                    <div className={styles.secondPart02}>
+                        <p className={styles.wordsPart1}>We Provide Quality</p>
+                        <p className={styles.wordsPart2}>At our poultry farm, we are dedicated to raising high-quality, healthy chickens in a sustainable and humane environment, where each bird is nurtured with care, fed a natural diet, and kept in spacious, clean coops, ensuring that every product we deliver, from fresh eggs to premium cuts, meets the highest standards of safety, taste, and nutrition for your family’s table.</p>
+                        <button className={styles.secondPartButton}>See Our Products</button>
+                    </div>
+                </div>
+            </div></>
+    )
+}
