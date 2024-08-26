cf71976a920df71d28b0298880c27fa5b96c1416
TEE2DWHY <isaacolorunfemi330@gmail.com>
2024-08-26 14:45:31 +0100

    merged main to others

--- src/pages/About/components/AboutBody2.tsx
+++ src/pages/About/components/AboutBody2.tsx
@@ -1,17 +1,16 @@
-import styles from "./styles/aboutbody2.module.scss"
+import styles from "./styles/aboutbody2.module.scss";
 
-export default function AboutBody2 () 
-{
-    return (
-        <>
-        <div className={styles.thirdPart}>
-            <div className={styles.thirdPartArea}>
-                 <p className={styles.thirdPartWord}>
-                     Our numbers prove the quality and starndard based service and products we offer
-                </p>
-            </div>
-            
+export default function AboutBody2() {
+  return (
+    <>
+      <div className={styles.thirdPart}>
+        <div className={styles.thirdPartArea}>
+          <p className={styles.thirdPartWord}>
+            Our numbers prove the quality and standard based service and
+            products we offer
+          </p>
         </div>
-        </>
-    )
+      </div>
+    </>
+  );
 }
